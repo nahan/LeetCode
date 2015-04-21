8d93fbb7a1e45571ad0ce14287a213f1387fa60c
Han Na <hosseni@hotmail.com>
2015-04-21 15:59:54 -0500

    Create an empty solution for question 65.

---
+++ LeetCode067/src/Solution.java
@@ -0,0 +1,17 @@
+
+/**
+ * Given two binary strings, return their sum (also a binary string).
+ * For example,
+ * a = "11"
+ * b = "1"
+ * Return "100".
+ */
+public class Solution {
+	public String addBinary(String a, String b) {
+		return null;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		
+	}
+}
