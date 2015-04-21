4d68d8a646084e00461289d40e9e759f9a8276f3
Han Na <hosseni@hotmail.com>
2015-04-21 16:30:47 -0500

    Add a variable of StringBuilder.

--- LeetCode067/src/Solution.java
+++ LeetCode067/src/Solution.java
@@ -8,10 +8,19 @@
  */
 public class Solution {
 	public String addBinary(String a, String b) {
+		if (a == null || b == null) {
+			return a == null ? b : a;
+		}
+		if (a.equals("") || b.equals("")) {
+			return a.equals("") ? b : a;
+		}
+		int length = a.length() > b.length() ? a.length() : b.length();
+		StringBuilder builder = new StringBuilder();
+		
 		return null;
 	}
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		
+		System.out.println(solution.addBinary("1", "111"));
 	}
 }
