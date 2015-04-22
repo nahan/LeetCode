ad22ec54ebf11adba9bd161bc01ac05089d7a40d
Han Na <hosseni@hotmail.com>
2015-04-21 19:23:49 -0500

    Create an empty solution for question 70. Mac

---
+++ LeetCode070/src/Solution.java
@@ -0,0 +1,27 @@
+
+/**
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+public class Solution {
+	public int climbStairs(int n) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.climbStairs(0));
+		System.out.println(solution.climbStairs(1));
+		System.out.println(solution.climbStairs(2));
+		System.out.println(solution.climbStairs(3));
+		System.out.println(solution.climbStairs(4));
+		System.out.println(solution.climbStairs(5));
+		System.out.println(solution.climbStairs(6));
+		System.out.println(solution.climbStairs(7));
+		System.out.println(solution.climbStairs(8));
+		System.out.println(solution.climbStairs(9));
+		System.out.println(solution.climbStairs(10));
+		System.out.println(solution.climbStairs(20));
+		System.out.println(solution.climbStairs(50));
+		System.out.println(solution.climbStairs(100));
+	}
+}
