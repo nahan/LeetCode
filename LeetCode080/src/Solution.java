bcfea4b66ee12b99768f1ef347e797c056fd1435
Han Na <hosseni@hotmail.com>
2015-04-21 23:54:50 -0500

    Still working on it. Win

--- LeetCode080/src/Solution.java
+++ LeetCode080/src/Solution.java
@@ -8,32 +8,54 @@
  */
 public class Solution {
 	public int removeDuplicates(int[] A) {
-		return 0;
+		if (A == null) {
+			return 0;
+		}
+		if (A.length < 3) {
+			return A.length;
+		}
+		int newLength = A.length;
+		for (int index = 1; index < newLength; index ++) {
+			if (A[index] == A[index - 1] && A[index] == A[index + 1]) {
+				removeItem(A, index);
+				newLength --;
+			}
+		}
+//		for (int i = 0; i < newLength; i ++) {
+//			System.out.print(A[i] + ", ");
+//		}
+//		System.out.println();
+		return newLength;
+	}
+	public void removeItem(int[] A, int index) {
+		for (int i = index; i < A.length - 1; i ++) {
+			A[i] = A[i + 1];
+		}
 	}
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		int [] array = null;
-		int[] array0 = new int[0];
-		int[] array1 = new int[1];
-		int[] array2 = new int[2];
-		int[] array3 = new int[3];
-		int[] array4 = new int[4];
-		int[] array5 = new int[5];
-		int[] array6 = new int[6];
-		int[] array7 = new int[7];
-		int[] array8 = new int[8];
-		int[] array9 = new int[9];
-		System.out.println(solution.removeDuplicates(array));
-		System.out.println(solution.removeDuplicates(array0));
-		System.out.println(solution.removeDuplicates(array1));
-		System.out.println(solution.removeDuplicates(array2));
-		System.out.println(solution.removeDuplicates(array3));
-		System.out.println(solution.removeDuplicates(array4));
-		System.out.println(solution.removeDuplicates(array5));
-		System.out.println(solution.removeDuplicates(array6));
-		System.out.println(solution.removeDuplicates(array7));
-		System.out.println(solution.removeDuplicates(array8));
-		System.out.println(solution.removeDuplicates(array9));
+		int[] array0 = {};
+		int[] array1 = {0};
+		int[] array2 = {0, 1};
+		int[] array3 = {0, 0, 0};
+		int[] array4 = {0, 0, 0, 1};
+		int[] array5 = {0, 0, 0, 1, 1};
+		int[] array6 = {0, 0, 0, 1, 1, 1};
+		int[] array7 = {0, 0, 0, 1, 1, 1, 1};
+		int[] array8 = {0, 0, 0, 1, 1, 1, 1, 2};
+		int[] array9 = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+		System.out.println("input: null, \t\t\t\texpected length: 0, result: " + solution.removeDuplicates(array));
+		System.out.println("input: {}, \t\t\t\texpected length: 0, result: " + solution.removeDuplicates(array0));
+		System.out.println("input: {0}, \t\t\t\texpected length: 1, result: " + solution.removeDuplicates(array1));
+		System.out.println("input: {0, 1}, \t\t\t\texpected length: 2, result: " + solution.removeDuplicates(array2));
+		System.out.println("input: {0, 0, 0}, \t\t\texpected length: 2, result: " + solution.removeDuplicates(array3));
+		System.out.println("input: {0, 0, 0, 1}, \t\t\texpected length: 3, result: " + solution.removeDuplicates(array4));
+		System.out.println("input: {0, 0, 0, 1, 1}, \t\texpected length: 4, result: " + solution.removeDuplicates(array5));
+		System.out.println("input: {0, 0, 0, 1, 1, 1}, \t\texpected length: 4, result: " + solution.removeDuplicates(array6));
+		System.out.println("input: {0, 0, 0, 1, 1, 1, 1}, \t\texpected length: 4, result: " + solution.removeDuplicates(array7));
+		System.out.println("input: {0, 0, 0, 1, 1, 1, 1, 2}, \texpected length: 5, result: " + solution.removeDuplicates(array8));
+		System.out.println("input: {0, 0, 0, 1, 1, 1, 2, 2, 2}, \texpected length: 6, result: " + solution.removeDuplicates(array9));
 		
 	}
 }
