eb2b87aed035e2a0df72faba0bc8bed71b11a6ff
Han Na <hosseni@hotmail.com>
2015-04-21 19:17:04 -0500

    Temporarily stop working on it, will resume later.

--- LeetCode065/src/Solution.java
+++ LeetCode065/src/Solution.java
@@ -10,5 +10,20 @@
  * Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
  */
 public class Solution {
-
+	public boolean isNumber(String s) {
+		if (s == null) {
+			return false;
+		} else {
+			System.out.println(s.length());
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.isNumber("0"));
+		System.out.println(solution.isNumber(" 0.1 "));
+		System.out.println(solution.isNumber("abc"));
+		System.out.println(solution.isNumber("1 a"));
+		System.out.println(solution.isNumber("2e10"));
+	}
 }
