c889ea24893d1b7487cd7f0410cec47fbf8acf17
Han Na <hosseni@hotmail.com>
2015-04-21 19:13:28 -0500

    Accepted.

--- LeetCode067/src/Solution.java
+++ LeetCode067/src/Solution.java
@@ -14,13 +14,41 @@ public class Solution {
 		if (a.equals("") || b.equals("")) {
 			return a.equals("") ? b : a;
 		}
-		int length = a.length() > b.length() ? a.length() : b.length();
-		StringBuilder builder = new StringBuilder();
+		if (a.length() < b.length()) {
+			return addBinary(b, a);
+		}
+		int indexA = a.length() - 1;
+		int indexB = b.length() - 1;
+		int[] digitArray = new int[a.length() + 1];
+		int indexDigit = digitArray.length - 1;
 		
-		return null;
+		while (indexA >= 0 && indexB >= 0) {
+			digitArray[indexDigit] = (int)a.charAt(indexA) - 48 + (int)b.charAt(indexB) - 48;
+			indexA --;
+			indexB --;
+			indexDigit --;
+		}
+		while (indexA >= 0) {
+			digitArray[indexDigit] = (int)a.charAt(indexA) - 48;
+			indexA --;
+			indexDigit --;
+		}
+		StringBuilder builder = new StringBuilder();
+		for (int i = digitArray.length - 1; i >= 0; i --) {
+			if (digitArray[i] >= 2) {
+				digitArray[i - 1] += digitArray[i] / 2;
+				digitArray[i] = digitArray[i] % 2;
+			}
+			if (i == 0 && digitArray[i] == 0) {
+				continue;
+			} else {
+				builder.append(digitArray[i]);
+			}
+		}
+		return builder.reverse().toString();
 	}
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.addBinary("1", "111"));
+		System.out.println(solution.addBinary("1111", "11"));
 	}
 }
