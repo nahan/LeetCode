9a04f642e198bc953bdf5f1165b2552d57b90c96
Han Na <hosseni@hotmail.com>
2015-04-21 22:56:53 -0500

    Create an empty solution for question 80. Win

---
+++ LeetCode080/src/Solution.java
@@ -0,0 +1,39 @@
+
+/**
+ * Follow up for "Remove Duplicates":
+ * What if duplicates are allowed at most twice?
+ * For example,
+ * Given sorted array A = [1,1,1,2,2,3],
+ * Your function should return length = 5, and A is now [1,1,2,2,3].
+ */
+public class Solution {
+	public int removeDuplicates(int[] A) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int [] array = null;
+		int[] array0 = new int[0];
+		int[] array1 = new int[1];
+		int[] array2 = new int[2];
+		int[] array3 = new int[3];
+		int[] array4 = new int[4];
+		int[] array5 = new int[5];
+		int[] array6 = new int[6];
+		int[] array7 = new int[7];
+		int[] array8 = new int[8];
+		int[] array9 = new int[9];
+		System.out.println(solution.removeDuplicates(array));
+		System.out.println(solution.removeDuplicates(array0));
+		System.out.println(solution.removeDuplicates(array1));
+		System.out.println(solution.removeDuplicates(array2));
+		System.out.println(solution.removeDuplicates(array3));
+		System.out.println(solution.removeDuplicates(array4));
+		System.out.println(solution.removeDuplicates(array5));
+		System.out.println(solution.removeDuplicates(array6));
+		System.out.println(solution.removeDuplicates(array7));
+		System.out.println(solution.removeDuplicates(array8));
+		System.out.println(solution.removeDuplicates(array9));
+		
+	}
+}
