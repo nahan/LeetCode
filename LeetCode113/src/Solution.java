bb45bfac8bf8408aeb87eb42d9d81fc41ef1360d
Han Na <hosseni@hotmail.com>
2015-04-22 23:47:51 -0500

    Add test case and methods. Win

--- LeetCode113/src/Solution.java
+++ LeetCode113/src/Solution.java
@@ -27,7 +27,66 @@ class TreeNode {
 	}
 }
 public class Solution {
-public List<List<Integer>> pathSum(TreeNode root, int sum) {
-     return null;   
-    }
+	public List<List<Integer>> pathSum(TreeNode root, int sum) {
+		return null;
+	}
+	public void preOrder(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.val + ", ");
+		if (root.left != null) {
+			preOrder(root.left);
+		}
+		if (root.right != null) {
+			preOrder(root.right);
+		}
+	}
+	public void inOrder(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		if (root.left != null) {
+			inOrder(root.left);
+		}
+		System.out.print(root.val + ", ");
+		if (root.right != null) {
+			inOrder(root.right);
+		}
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		
+		TreeNode node01 = new TreeNode(5);
+		TreeNode node02 = new TreeNode(4);
+		TreeNode node03 = new TreeNode(8);
+		TreeNode node04 = new TreeNode(11);
+		TreeNode node05 = new TreeNode(13);
+		TreeNode node06 = new TreeNode(4);
+		TreeNode node07 = new TreeNode(7);
+		TreeNode node08 = new TreeNode(2);
+		TreeNode node09 = new TreeNode(5);
+		TreeNode node10 = new TreeNode(1);
+		
+		node01.left = node02;
+		node01.right = node03;
+		
+		node02.left = node04;
+		
+		node03.left = node05;
+		node03.right = node06;
+		
+		node04.left = node07;
+		node04.right = node08;
+		
+		node06.left = node09;
+		node06.right = node10;
+		
+		solution.preOrder(node01);
+		System.out.println();
+		solution.inOrder(node01);
+		System.out.println();
+		
+		System.out.println(solution.pathSum(node01, 22));
+	}
 }
