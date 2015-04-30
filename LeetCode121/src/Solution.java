229c09ee093a91e30e23e8980ed2c0a4f1a3dc47
Han Na <hosseni@hotmail.com>
2015-04-30 03:35:32 -0500

    Create an empty solution for 121. Mac

---
+++ LeetCode121/src/Solution.java
@@ -0,0 +1,14 @@
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum 
+ */
+public class Solution {
+	public int maxProfit(int[] prices) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.maxProfit(null));
+	}
+}
