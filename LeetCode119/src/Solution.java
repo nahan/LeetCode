f2d8e856e5908e10908a228e78b464c7a050e78e
Han Na <hosseni@hotmail.com>
2015-04-30 01:19:14 -0500

    Accepted. Mac

--- LeetCode119/src/Solution.java
+++ LeetCode119/src/Solution.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Given an index k, return the kth row of the Pascal's triangle.
  * For example, given k = 3,
  * Return [1,3,3,1].
+ * 
+ * Note:
+ * Could you optimize your algorithm to use only O(k) extra space?
  */
 public class Solution {
 	public List<Integer> getRow(int rowIndex) {
-		return null;
+		List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+		List<Integer> initial = new ArrayList<Integer>();
+		List<Integer> previous = initial;
+		initial.add(1);
+		triangle.add(initial);
+		
+		int index = 1;
+		while (index <= rowIndex) {
+			List<Integer> current = new ArrayList<Integer>();
+			current.add(1);
+			for (int i = 1; i < previous.size(); i ++) {
+				current.add(previous.get(i - 1) + previous.get(i));
+			}
+			current.add(1);
+			triangle.add(current);
+			previous = current;
+			index ++;
+		}
+		System.out.println(triangle);
+		return triangle.get(rowIndex);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello I am back!");
+		Solution solution = new Solution();
+		System.out.println(solution.getRow(7));
 	}
 }
