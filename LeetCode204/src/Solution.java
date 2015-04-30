aa1f91580a519aa2b907dfcd9ed41b1f3866bd10
Han Na <hosseni@hotmail.com>
2015-04-30 12:17:49 -0500

    "Time Limit Exceeded". Mac

--- LeetCode204/src/Solution.java
+++ LeetCode204/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 
 /**
  * Count the number of prime numbers less than a non-negative number, n
@@ -5,20 +7,24 @@
 public class Solution {
 	public int countPrimes(int n) {
 		int count = 0;
+		HashMap<Integer, Boolean> primeMap = new HashMap<Integer, Boolean>();
 		for (int i = 2; i < n; i ++) {
-			if (isPrime(i) == true) {
+			primeMap.put(i, true);
+		}
+		int m = (int) Math.sqrt(n);
+		for (int i = 2; i <= m; i ++) {
+			if (primeMap.get(i) == true) {
+				for (int k = i * i; k < n; k += i) {
+					primeMap.put(k, false);
+				}
+			}
+		}
+		for (int i = 2; i < n; i ++) {
+			if (primeMap.get(i) == true) {
 				count += i;
 			}
 		}
 		return count;
-	}
-	public boolean isPrime(int n) {
-		for (int i = 2; i < n; i ++) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		return true;
 	}
 	public static void main(String[] args) {
 		Solution solution = new Solution();
@@ -35,6 +41,7 @@ public class Solution {
 		System.out.println(solution.countPrimes(10));
 		System.out.println(solution.countPrimes(100));
 		System.out.println(solution.countPrimes(499979));
+		System.out.println(solution.countPrimes(1500000));
 
 	}
 }
