e137ee0b719c5002382249fc52db81294d0f4d8e
Han Na <hosseni@hotmail.com>
2015-05-03 13:39:02 -0500

    Create an empty solution for problem 12. Mac

---
+++ LeetCode012/src/Solution.java
@@ -0,0 +1,22 @@
+
+/**
+ * Given an integer, convert it to a roman numeral.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+public class Solution {
+	public String intToRoman(int num) {
+		return null;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int test00 = 1;
+		int test01 = 5;
+		int test02 = 10;
+		int test03 = 3999;
+		
+		System.out.println(test00 + " is: " + solution.intToRoman(test00));
+		System.out.println(test01 + " is: " + solution.intToRoman(test01));
+		System.out.println(test02 + " is: " + solution.intToRoman(test02));
+		System.out.println(test03 + " is: " + solution.intToRoman(test03));
+	}
+}
