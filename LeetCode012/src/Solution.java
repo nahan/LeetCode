25a35f5fb0e39daebed015934919e519d364d1b6
Han Na <hosseni@hotmail.com>
2015-05-04 12:37:00 -0500

    Accepted. Win

--- LeetCode012/src/Solution.java
+++ LeetCode012/src/Solution.java
@@ -5,6 +5,9 @@
  */
 public class Solution {
 	public String intToRoman(int num) {
+		if (num < 1 || num > 3999) {
+			return null;
+		}
 		StringBuilder builder = new StringBuilder();
 		int temp = num;
 		int[] digit = new int[4];
@@ -35,10 +38,9 @@ public class Solution {
 				builder.append("C");
 				hundred --;
 			}
-		} else {
-			if (hundred > 3) {
-				builder.append("D");
-			}
+		} else if (hundred == 4) {
+			builder.append("CD");
+		} else if (hundred >= 0) {
 			while (hundred > 0) {
 				builder.append("C");
 				hundred --;
@@ -49,15 +51,14 @@ public class Solution {
 		if (ten == 9) {
 			builder.append("XC");
 		} else if (ten >= 5) {
+			builder.append("L");
 			while (ten > 5) {
 				builder.append("X");
 				ten --;
 			}
-			builder.append("L");
-		} else {
-			if (ten > 3) {
-				builder.append("L");
-			}
+		} else if (ten == 4) {
+			builder.append("XL");
+		} else if (ten >= 0) {
 			while (ten > 0) {
 				builder.append("X");
 				ten --;
@@ -66,24 +67,22 @@ public class Solution {
 		
 		// unit
 		if (unit == 9) {
-			builder.append("XI");
+			builder.append("IX");
 		} else if (unit >= 5) {
+			builder.append("V");
 			while (unit > 5) {
 				builder.append("I");
 				unit --;
 			}
-			builder.append("V");
-		} else {
-			if (unit > 3) {
-				builder.append("V");
-			}
+		} else if (unit == 4) {
+			builder.append("IV");
+		} else if (unit >= 0) {
 			while (unit > 0) {
 				builder.append("I");
 				unit --;
 			}
 		}
 		
-//		System.out.println(digit[0] + ", " + digit[1] + ", " + digit[2] + ", " + digit[3]);
 		return builder.toString();
 	}
 	public static void main(String[] args) {
@@ -91,6 +90,17 @@ public class Solution {
 		for (int i = 1; i <= 100; i ++) {
 			System.out.println(i + " is: " + solution.intToRoman(i));
 		}
+		System.out.println("501 is " + solution.intToRoman(501));
+		System.out.println("550 is " + solution.intToRoman(550));
+		System.out.println("530 is " + solution.intToRoman(530));
+		System.out.println("707 is " + solution.intToRoman(707));
+		System.out.println("890 is " + solution.intToRoman(890));
+		System.out.println("1500 is " + solution.intToRoman(1500));
+		System.out.println("1800 is " + solution.intToRoman(1800));
+		System.out.println("900 is " + solution.intToRoman(900));
+		System.out.println("3999 is " + solution.intToRoman(3999));
 		
+		System.out.println("0 is " + solution.intToRoman(0));
+		System.out.println("4000 is " + solution.intToRoman(4000));
 	}
 }
