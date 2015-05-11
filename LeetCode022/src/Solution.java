ffaf5f51b71438818c0b5f637b5c7a0cd90b4484
Han Na <hosseni@hotmail.com>
2015-05-11 02:34:06 -0500

    Still working on it, I will kill you Backtracking, like Dynamic Programming. Win

--- LeetCode022/src/Solution.java
+++ LeetCode022/src/Solution.java
@@ -2,25 +2,24 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
 /**
- * Given n pairs of parentheses, 
- * write a function to generate all combinations of well-formed parentheses.
- * For example, given n = 3, a solution set is:
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses. For example, given n = 3, a solution set is:
  * "((()))", "(()())", "(())()", "()(())", "()()()"
  */
 public class Solution {
 	public List<String> generateParenthesis(int n) {
 		List<String> result = new ArrayList<String>();
-		for (int i = 0; i < n; i ++) {
+		for (int i = 0; i < n; i++) {
 			if (result.isEmpty() == true) {
 				result.add("()");
 			} else {
 				List<String> subResult = new ArrayList<String>();
 				String currentParenthesis = null;
-				for (String item: result) {
+				for (String item : result) {
 					currentParenthesis = item;
-					addItems(subResult, addParenthesis(currentParenthesis).iterator());
+					addItems(subResult, addParenthesis(currentParenthesis)
+							.iterator());
 				}
 				result.clear();
 				addItems(result, subResult.iterator());
@@ -28,6 +27,7 @@ public class Solution {
 		}
 		return result;
 	}
+
 	public List<String> addParenthesis(String parenthesis) {
 		List<String> result = new ArrayList<String>();
 		result.add("()" + parenthesis);
@@ -35,6 +35,7 @@ public class Solution {
 		result.add("(" + parenthesis + ")");
 		return result;
 	}
+
 	public void addItems(List<String> list, Iterator<String> iterator) {
 		while (iterator.hasNext()) {
 			String item = iterator.next();
@@ -43,11 +44,42 @@ public class Solution {
 			}
 		}
 	}
+
+	public List<String> generateParenthesisBT(int n) {
+		List<String> result = new ArrayList<String>();
+		char[] parenthesis = new char[n * 2];
+		
+		for (int index = 1; index < n; index++) {
+			parenthesis[0] = '(';
+			for (int i = 1; i < parenthesis.length; i ++) {
+				parenthesis[i] = ')';
+			}
+			System.out.println(charsToString(parenthesis));
+			findValidPosition(index, parenthesis);
+			System.out.println(charsToString(parenthesis));
+		}
+		return result;
+	}
+	public void findValidPosition(int index, char[] parenthesis) {
+		if (index == parenthesis.length - 1) {
+			
+		}
+	}
+	public String charsToString(char[] charList) {
+		StringBuilder builder = new StringBuilder();
+		for (char item: charList) {
+			builder.append(item);
+		}
+		return builder.toString();
+	}
+
+	public boolean isValid(int index) {
+		return false;
+	}
+
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		for (int i = 0; i < 11; i ++) {
-			System.out.println(solution.generateParenthesis(i));
-		}
-		
+		System.out.println(solution.generateParenthesisBT(1));
+
 	}
 }
