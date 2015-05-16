63ee952c692a3b0f241f83ffb6200b63d7a4f4c1
Han Na <hosseni@hotmail.com>
2015-05-15 22:25:18 -0500

    Accepted. EPI. Win

--- LeetCode022/src/Solution.java
+++ LeetCode022/src/Solution.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * Given n pairs of parentheses, write a function to generate all combinations
  * of well-formed parentheses. For example, given n = 3, a solution set is:
  * "((()))", "(()())", "(())()", "()(())", "()()()"
+ */
+
+/**
+ * suppose we have a string whose length is less than 2k, 
+ * and we know that string can be completed to a string with k pairs of matched parens.
+ * How can we extend that string with an additional character
+ * so that the resulting string can still be completed to a string with k pairs of matched parens?
+ * 
+ * There are two possibilities:
+ * we add a left parens, or we add a right parens.
+ * 
+ * 1. If we add a left parens, and still want to complete the string to a string with k pairs of matched parens,
+ * it must be that the number of left parens we need is greater than 0;
+ * 
+ * 2. If we add a right parens, and still want to complete the string to a string with k pairs of matched parens,
+ * if must be that the number of left parens we need is less than the number of the needed right parens.
+ * i.e., there are unmathced left parens in the string.
  */
 public class Solution {
 	public List<String> generateParenthesis(int n) {
@@ -26,102 +41,6 @@ public class Solution {
 			directedGenerateBalancedParenthsises(leftParensNeeded, rightParensNeeded - 1, validPrefix + ")", result);
 		}
 	}
-	public List<String> generateParenthesis1(int n) {
-		List<String> result = new ArrayList<String>();
-		for (int i = 0; i < n; i++) {
-			if (result.isEmpty() == true) {
-				result.add("()");
-			} else {
-				List<String> subResult = new ArrayList<String>();
-				String currentParenthesis = null;
-				for (String item : result) {
-					currentParenthesis = item;
-					addItems(subResult, addParenthesis(currentParenthesis)
-							.iterator());
-				}
-				result.clear();
-				addItems(result, subResult.iterator());
-			}
-		}
-		return result;
-	}
-
-	public List<String> addParenthesis(String parenthesis) {
-		List<String> result = new ArrayList<String>();
-		result.add("()" + parenthesis);
-		result.add(parenthesis + "()");
-		result.add("(" + parenthesis + ")");
-		return result;
-	}
-
-	public void addItems(List<String> list, Iterator<String> iterator) {
-		while (iterator.hasNext()) {
-			String item = iterator.next();
-			if (list.contains(item) == false) {
-				list.add(item);
-			}
-		}
-	}
-
-	public List<String> generateParenthesisBT(int n) {
-		List<String> result = new ArrayList<String>();
-		char[] parenthesis = new char[n * 2];
-		
-		for (int index = 1; index < n; index++) {
-			parenthesis[0] = '(';
-			for (int i = 1; i < parenthesis.length; i ++) {
-				parenthesis[i] = ')';
-			}
-			System.out.println("here: " + index);
-			System.out.println("before: " + charsToString(parenthesis));
-			int time = 1;
-			findValidPosition(index, parenthesis, result, time);
-			System.out.println("after: " + charsToString(parenthesis));
-		}
-		return result;
-	}
-	public void findValidPosition(int index, char[] parenthesis, List<String> result, int time) {
-		System.out.println(charsToString(parenthesis));
-		if (index == parenthesis.length - 1) {
-			System.out.println("yes, " + charsToString(parenthesis));
-			result.add(charsToString(parenthesis));
-			return;
-		}
-		for (int i = index; i < parenthesis.length - 1; i ++) {
-			if (isValid(i, parenthesis, time) == true) {
-				parenthesis[i] = '(';
-				findValidPosition(i + 1, parenthesis, result, time + 1);
-				parenthesis[i] = ')';
-			}
-		}
-	}
-	public boolean isValid(int index, char[] parenthesis, int time) {
-		if (time > parenthesis.length / 2) {
-			return false;
-		}
-		Stack<Character> temp = new Stack<Character>();
-		temp.push(parenthesis[0]);
-		for (int i = 1; i < index; i ++) {
-			if (parenthesis[i] == '(') {
-				temp.push(parenthesis[i]);
-			} else {
-				if (temp.empty() == true) {
-					return false;
-				} else {
-					temp.pop();
-				}
-			}
-		}
-		return temp.contains(')') == false;
-	}
-	public String charsToString(char[] charList) {
-		StringBuilder builder = new StringBuilder();
-		for (char item: charList) {
-			builder.append(item);
-		}
-		return builder.toString();
-	}
-
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		System.out.println(solution.generateParenthesis(4));
