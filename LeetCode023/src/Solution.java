3419dc41c0961c98c0a7370a1c260434c5bdf9ce
Han Na <hosseni@hotmail.com>
2015-05-17 23:55:10 -0500

    Create an empty solution for problem 23. Mac

---
+++ LeetCode023/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ */
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+public class Solution {
+	public ListNode mergeKLists(ListNode[] lists) {
+		return null;
+	}
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+	}
+}
