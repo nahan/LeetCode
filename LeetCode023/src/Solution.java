8bd94b8ea99bc63a90a9cfeef6fccc49b9dd0d77
Han Na <hosseni@hotmail.com>
2015-05-22 00:34:55 -0500

    Gonna use heap to solve this problem. Win

--- LeetCode023/src/Solution.java
+++ LeetCode023/src/Solution.java
@@ -17,43 +17,9 @@ public class Solution {
 		if (lists.length == 1) {
 			return lists[0];
 		}
+		
 		ListNode head = null;
-		int previous = 0;
-		int current = previous + 1;
-		while (previous < lists.length) {
-			head = lists[previous];
-			if (head != null) {
-				break;
-			}
-			previous ++;
-			current ++;
-		}
-		ListNode item = null;
-		while (current < lists.length) {
-			item = lists[current];
-			if (item != null) {
-				head = mergeTwoLists(head, item);
-			}
-			current ++;
-		}
 		return head;
-	}
-	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-		ListNode head = new ListNode(0);
-		ListNode tail = head;
-		while (list1 != null && list2 != null) {
-			if (list1.val < list2.val) {
-				tail.next = list1;
-				tail = list1;
-				list1 = list1.next;
-			} else {
-				tail.next = list2;
-				tail = list2;
-				list2 = list2.next;
-			}
-		}
-		tail.next = list1 != null ? list1 : list2;
-		return head.next;
 	}
 	public void printList(ListNode node) {
 		if (node != null) {
