4a32bb0afd2a2f46a041929705e9b4b13e6ad665
Han Na <hosseni@hotmail.com>
2015-05-31 19:24:12 -0500

    Still "Time Limit Exceeded". Win

--- LeetCode030/src/Solution.java
+++ LeetCode030/src/Solution.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -18,6 +19,68 @@ import java.util.TreeMap;
  */
 public class Solution {
 	public List<Integer> findSubstring(String s, String[] words) {
+		List<Integer> result = new ArrayList<Integer>();
+		if (s == null || s.length() == 0 || words == null || words.length == 0) {
+			return result;
+		}
+		
+		int totalLength = 0;
+		HashMap<Integer, Integer> stringHash = new HashMap<Integer, Integer>();
+		for (int i = 0; i < words.length; i ++) {
+			totalLength += words[i].length();
+			stringHash.put(i, -1);
+		}
+//		System.out.println(stringHash);
+		
+		// got a two pointers solution!!!!!!!!!!!!!!!!!!!!!!!
+		TreeMap<Integer, String> lengthMap = new TreeMap<Integer, String>();
+		int from = 0;
+		while (from <= s.length() - totalLength) {
+//			System.out.println("this time we start from: " + from);
+			int headIndex = s.length() - 1;
+			int tailIndex = 0;
+			for (int i = 0; i < words.length; i ++) {
+				int index = findIndex(s, from, words[i], lengthMap);
+				if (index != -1) {
+					lengthMap.put(index, words[i]);
+					headIndex = headIndex < index ? headIndex : index;
+					tailIndex = tailIndex > index + words[i].length() ? tailIndex : index + words[i].length();
+				}
+			}
+//			System.out.println(lengthMap);
+//			System.out.println("head: " + headIndex + ", tail: " + tailIndex);
+			if (lengthMap.size() != words.length) {
+				lengthMap.clear();
+				break;
+			}
+			int previousIndex = headIndex;
+			StringBuilder subString = new StringBuilder();
+			Iterator<String> iter = lengthMap.values().iterator();
+			while (iter.hasNext()) {
+				String current = iter.next();
+				subString.append(current);
+				if (s.indexOf(subString.toString(), from) == headIndex) {
+					if (subString.toString().length() == totalLength) {
+//						System.out.println("got it");
+						result.add(headIndex);
+						from = previousIndex + current.length();
+						lengthMap.clear();
+						break;
+					} else {
+//						System.out.println("continue");
+						previousIndex += current.length();
+					}
+				} else {
+//					System.out.println("break here, " + subString.toString() + ", " + s.indexOf(subString.toString()));
+					from = previousIndex;
+					lengthMap.clear();
+					break;
+				}
+			}
+		}
+		return result;
+	}
+	public List<Integer> findSubstring2(String s, String[] words) {
 		List<Integer> result = new ArrayList<Integer>();
 		if (s == null || s.length() == 0 || words == null || words.length == 0) {
 			return result;
@@ -112,7 +175,7 @@ public class Solution {
 		
 		String s6 = "abcd111de111abfgabcd111abcd";
 		String[] words6 = {"abcd", "111"};
-		System.out.println(s6 + ", excepted: [0, 16], output: " + solution.findSubstring(s6, words6));
+		System.out.println(s6 + ", excepted: [0, 16, 20], output: " + solution.findSubstring(s6, words6));
 		
 		String s7 = "abcdefg";
 		String[] words7 = {"cd"};
