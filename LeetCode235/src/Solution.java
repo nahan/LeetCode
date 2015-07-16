16a32cd3b8ec91ac55e57698076ab1f1576681d8
Han Na <hosseni@hotmail.com>
2015-07-16 15:27:28 -0500

    Create an empty solution for 235.

---
+++ LeetCode235/src/Solution.java
@@ -0,0 +1,33 @@
+
+/**
+ * Given a binary search tree (BST), 
+ * find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * 
+          _______6______
+         /              \
+     ___2__          ___8__
+    /      \        /      \
+    0      _4_     7        9
+          /   \
+         3     5
+ *
+ * For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. 
+ * Another example is LCA of nodes 2 and 4 is 2, 
+ * since a node can be a descendant of itself according to the LCA definition.
+ */
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+ }
+public class Solution {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, I am back!");
+	}
+}
