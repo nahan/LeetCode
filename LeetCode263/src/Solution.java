13c59cc28f33c76b5eb439a4f148de6c69de66b7
Han Na <hosseni@hotmail.com>
2015-08-28 00:10:08 -0400

    Add test cases. Win

--- LeetCode263/src/Solution.java
+++ LeetCode263/src/Solution.java
@@ -8,13 +8,16 @@
  */
 public class Solution {
 	public boolean isUgly(int num) {
+		if (num <= 1) {
+			return num == 1 ? true : false;
+		}
         return false;
     }
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
 		Solution solution = new Solution();
-		for (int i = 1; i <= 100; i ++) {
+		for (int i = -1; i <= 100; i ++) {
 			System.out.println(solution.isUgly(i));
 		}
 	}
