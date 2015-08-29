baeaff186038191fe17b24234148f640ed6e42d5
Han Na <hosseni@hotmail.com>
2015-08-29 01:03:05 -0400

    Wrong Answer. Win

--- LeetCode257/src/Solution.java
+++ LeetCode257/src/Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,8 +28,54 @@ class TreeNode {
 }
 public class Solution {
 	public List<String> binaryTreePaths(TreeNode root) {
-		return null;
+		List<String> result = new ArrayList<String>();
+		if (root == null) {
+			return result;
+		}
+		
+		this.printLeafValue(root);
+		System.out.println();
+		
+		List<TreeNode> nodeList = new ArrayList<TreeNode>();
+		getPaths(root, nodeList);
+		for (TreeNode item: nodeList) {
+			if (item.equals(root)) {
+				result.add(String.valueOf(item.val));
+			} else {
+				result.set(result.size() - 1, result.get(result.size() - 1) + "->" + String.valueOf(item.val));
+			}
+		}
+		return result;
 	}
+	public void getPaths(TreeNode node, List<TreeNode> list) {
+		if (node.left != null) {
+			list.add(node);
+			getPaths(node.left, list);
+		}
+		if (node.right != null) {
+			list.add(node);
+			getPaths(node.right, list);
+		}
+		if (node.left == null && node.right == null) {
+			list.add(node);
+		}
+	}
+	
+	public void printLeafValue(TreeNode node) {
+		if (node.left != null) {
+			System.out.print(node.val + ", ");
+			printLeafValue(node.left);
+		}
+		if (node.right != null) {
+			System.out.print(node.val + ", ");
+			printLeafValue(node.right);
+		}
+		
+		if (node.left == null && node.right == null) {
+			System.out.print(node.val + ", ");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
@@ -37,10 +84,12 @@ public class Solution {
 		TreeNode node00 = new TreeNode(2);
 		TreeNode node01 = new TreeNode(3);
 		TreeNode node02 = new TreeNode(5);
+		TreeNode node03 = new TreeNode(6);
 		
 		root.left = node00;
 		root.right = node01;
-		node00.right = node02;
+		node00.right = node03;
+		node00.left = node02;
 		
 		System.out.println(solution.binaryTreePaths(root));
 	}
