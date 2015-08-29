67ba75828f1320353b264d39f35047969e15812b
Han Na <hosseni@hotmail.com>
2015-08-28 20:51:51 -0400

    Accepted but Brute Force. Win

--- LeetCode258/src/Solution.java
+++ LeetCode258/src/Solution.java
@@ -12,10 +12,26 @@
  */
 public class Solution {
 	public int addDigits(int num) {
-        return 0;
+		if (num < 10) {
+			return num;
+		}
+		int temp = 0;
+		while (num > 0) {
+			temp += getDigit(num);
+			num /= 10;
+		}
+        return addDigits(temp);
     }
+	public int getDigit(int num) {
+		return num >= 10 ? num % 10 : num;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		Solution solution = new Solution();
+		for (int i = 0; i <= 1024; i ++) {
+			System.out.println(i + " -> " + solution.addDigits(i));
+		}
 	}
 }
