241c0401ccba201e8277f87a82d42564dacc97f2
Han Na <hosseni@hotmail.com>
2015-08-30 22:03:53 -0400

    Create an empty solution for problem 172. Win

---
+++ LeetCode172/src/Solution.java
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * 
+ * Note: Your solution should be in logarithmic time complexity.
+ * @author Han
+ *
+ */
+public class Solution {
+	public int trailingZeroes(int n) {
+        return 0;
+    }
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		for (int i = 0; i < 101; i ++) {
+			System.out.println(solution.trailingZeroes(i));
+		}
+	}
+}
