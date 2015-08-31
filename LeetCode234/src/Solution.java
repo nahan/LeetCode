9be657f4a958447f97520c4dcc172313682d7101
Han Na <hosseni@hotmail.com>
2015-08-31 17:35:37 -0400

    Accepted. Win

--- LeetCode234/src/Solution.java
+++ LeetCode234/src/Solution.java
@@ -17,13 +17,85 @@ class ListNode {
 }
 public class Solution {
 	public boolean isPalindrome(ListNode head) {
-		return false;
+		if (head == null || head.next == null) {
+			return true;
+		}
+		
+		boolean flag = true;
+		int size = getListSize(head);
+		
+		ListNode first = head;
+		ListNode preMid = this.getPreMid(size, head);
+		ListNode mid = null;
+		ListNode posMid = null;
+		
+		if (size % 2 == 0) {
+			posMid = preMid.next; 
+			preMid.next = null;
+		} else {
+			mid = preMid.next;
+			posMid = mid.next;
+			preMid.next = null;
+			mid.next = null;
+		}
+		
+		ListNode second = this.reverseList(posMid);
+		
+		ListNode firstIndex = first;
+		ListNode secondIndex = second;
+		while (firstIndex != null && secondIndex != null) {
+			if (firstIndex.val != secondIndex.val) {
+				flag = false;
+				break;
+			}
+			firstIndex = firstIndex.next;
+			secondIndex = secondIndex.next;
+		}
+		
+		posMid = this.reverseList(second);
+		if (mid != null) {
+			mid.next = posMid;
+			preMid.next = mid;
+		} else {
+			preMid.next = posMid;
+		}
+		
+		return flag;
     }
+	public int getListSize(ListNode head) {
+		if (head != null) {
+			return this.getListSize(head.next) + 1;
+		} else {
+			return 0;
+		}
+	}
+	public ListNode getPreMid(int size, ListNode head) {
+		int index = 0;
+		ListNode pointer = head;
+		while (index != size / 2 - 1) {
+			pointer = pointer.next;
+			index ++;
+		}
+		return pointer;
+	}
+	public ListNode reverseList(ListNode head) {
+		ListNode pre = head;
+		ListNode cur = head;
+		ListNode pos = head.next;
+		
+		pre.next = null;
+		while (pos != null) {
+			cur = pos;
+			pos = pos.next;
+			cur.next = pre;
+			pre = cur;
+		}
+		return cur;
+	}
 	public void print(ListNode node) {
 		if (node != null) {
 			System.out.print(node.val + " -> ");
 			this.print(node.next);
-			System.out.print(node.val + " -> ");
 		}
 	}
 	public void reversePrint(ListNode node) {
@@ -39,21 +111,27 @@ public class Solution {
 		ListNode node01 = new ListNode(1);
 		ListNode node02 = new ListNode(2);
 		ListNode node03 = new ListNode(3);
-		ListNode node04 = new ListNode(4);
-		ListNode node05 = new ListNode(5);
+		ListNode node04 = new ListNode(2);
+		ListNode node05 = new ListNode(1);
 		
 		node00.next = node01;
 		node01.next = node02;
 		node02.next = node03;
 		node03.next = node04;
 		node04.next = node05;
+		node05.next = new ListNode(0);
 		
 		solution.print(node00);
 		System.out.println();
-		//solution.reversePrint(node00);
-		System.out.println();
+//		solution.reversePrint(node00);
+//		System.out.println();
 		
 		System.out.println(solution.isPalindrome(null));
 		System.out.println(solution.isPalindrome(node00));
+		
+		solution.print(node00);
+		System.out.println();
+//		solution.reversePrint(node00);
+//		System.out.println();
 	}
 }
