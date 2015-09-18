fc38eed04d5aa53d82ee0dc2736f6aee5adf73d3
Han Na <hosseni@hotmail.com>
2015-09-17 21:35:58 -0400

    Accepted. Win

--- LeetCode204/src/Solution.java
+++ LeetCode204/src/Solution.java
@@ -5,7 +5,41 @@ import java.util.HashMap;
  * Count the number of prime numbers less than a non-negative number, n
  */
 public class Solution {
-	public int countPrimes(int n) {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] primes = new boolean[n + 1];
+        for (int i = 2; i < n; i++) {
+            primes[i] = true;
+        }
+        int m = (int)Math.sqrt(n);
+        for (int i = 2; i <= m; i++) {
+            if (primes[i] == true) {
+                for (int k = i * i; k <= n; k += i) {
+                    primes[k] = false;
+                }
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (primes[i] == true) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+    public int countPrimes1(int n) {
+        int count = 0;
+        boolean[] primes = new boolean[n + 1];
+        for (int i = 2; i < n; i++) {
+            if (primes[i] == false) {
+                count ++;
+            }
+            for (int j = i; j < n; j += i) {
+                primes[j] = true;
+            }
+        }
+        return count;
+    }
+	public int countPrimes0(int n) {
 		int count = 0;
 		HashMap<Integer, Boolean> primeMap = new HashMap<Integer, Boolean>();
 		for (int i = 2; i < n; i ++) {
@@ -31,6 +65,7 @@ public class Solution {
 		System.out.println(solution.countPrimes(0));
 		System.out.println(solution.countPrimes(1));
 		System.out.println(solution.countPrimes(2));
+		System.out.println(solution.countPrimes1(2));
 		System.out.println(solution.countPrimes(3));
 		System.out.println(solution.countPrimes(4));
 		System.out.println(solution.countPrimes(5));
@@ -42,6 +77,7 @@ public class Solution {
 		System.out.println(solution.countPrimes(100));
 		System.out.println(solution.countPrimes(499979));
 		System.out.println(solution.countPrimes(1500000));
+		System.out.println(solution.countPrimes1(1500000));
 
 	}
 }
