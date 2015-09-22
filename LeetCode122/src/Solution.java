22a45bcf7bba1d35c3c113f8f7b24d071f0709ba
Han Na <hosseni@hotmail.com>
2015-09-21 21:32:39 -0400

    TLE. Win

---
+++ LeetCode122/src/Solution.java
@@ -0,0 +1,55 @@
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. 
+ * You may complete as many transactions as you like 
+ * (ie, buy one and sell one share of the stock multiple times). 
+ * However, you may not engage in multiple transactions at the same time 
+ * (ie, you must sell the stock before you buy again).
+ * @author Han
+ *
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int result = 0;
+        int[] profits = new int[prices.length];
+        for (int i = 0; i < prices.length;) {
+            int max = 0;
+            int j = i;
+            int s = 0;
+            while (j < prices.length) {
+                profits[j] = prices[j] - prices[i];
+                if (max <= profits[j]) {
+                    max = profits[j];
+                    s = j;
+                }
+                j++;
+            }
+            i = s + 1;
+            result += max;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] p00 = null;
+        int[] p01 = {};
+        int[] p02 = {1};
+        int[] p03 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] p04 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] p05 = {1, 9, 2, 8, 3, 7, 4, 6, 5};
+        int[] p06 = {1, 7, 2, 5, 3};
+        int[] p07 = {1, 2, 3, 4, 5, 6};
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p00));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p01));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p02));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p03));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p04));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p05));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p06));
+        System.out.format("Max Profit: %d\n", solution.maxProfit(p07));
+    }
+}
