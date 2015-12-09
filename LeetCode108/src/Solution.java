7bf1254e9b3c2f8da50167f6d6a5c9eb719e4f54
Han Na <hosseni@hotmail.com>
2015-12-08 19:50:00 -0500

    Code Cleaned. Accepted. Win

--- LeetCode108/src/Solution.java
+++ LeetCode108/src/Solution.java
@@ -13,26 +13,19 @@ class TreeNode {
 	}
 }
 public class Solution {
-	public TreeNode sortedArrayToBST(int[] num) {
-		if (num == null || num.length == 0) {
-			return null;
-		}
-		int mid = num.length / 2;
-		TreeNode root = new TreeNode(num[mid]);
-		root.left = sortedArrayToBST(num, 0, mid - 1);
-		root.right = sortedArrayToBST(num, mid + 1, num.length - 1);
-		return root;
-	}
-	public TreeNode sortedArrayToBST(int[] num, int startAt, int endAt) {
-		if (num == null || num.length == 0 || endAt < startAt) {
-			return null;
-		}
-		int mid = startAt + (endAt - startAt + 1) / 2;
-		TreeNode node = new TreeNode(num[mid]);
-		node.left = sortedArrayToBST(num, startAt, mid - 1);
-		node.right = sortedArrayToBST(num, mid + 1, endAt);
-		return node;
-	}
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return nums == null? null: this.sortedArrayToBSTAux(nums, 0, nums.length - 1);
+    }
+    public TreeNode sortedArrayToBSTAux(int[] nums, int start, int end) {
+        TreeNode node = null;
+        if (start <= end) {
+            int mid = start + (end - start) / 2;
+            node = new TreeNode(nums[mid]);
+            node.left = this.sortedArrayToBSTAux(nums, start, mid - 1);
+            node.right = this.sortedArrayToBSTAux(nums, mid + 1, end);
+        }
+        return node;
+    }
 	public void preOrder(TreeNode root) {
 		if (root == null) {
 			return;
