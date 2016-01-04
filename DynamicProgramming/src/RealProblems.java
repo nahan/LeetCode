90b2b9397701c6d43308b0de6614541acb0a8ab5
Han Na <hosseni@hotmail.com>
2016-01-04 16:11:25 -0500

    309. Best Time to Buy and Sell Stock with Cooldown Accepted. Win this solution is referred to the discussion.

--- DynamicProgramming/src/RealProblems.java
+++ DynamicProgramming/src/RealProblems.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 
 public class RealProblems {
-    /**************************** Best Time to Buy and Sell Stock with Cooldown ****************************/
+    /**************************** 309. Best Time to Buy and Sell Stock with Cooldown ****************************/
     /**
      * Say you have an array for which the ith element is the price of a given stock on day i.
      * Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
@@ -16,16 +16,21 @@ public class RealProblems {
      * @return
      */
     public int maxProfitCool(int[] prices) {
-        if (prices == null || prices.length <= 2) {
-            if (prices.length == 2) {
-                return prices[1] - prices[0] > 0? prices[1] - prices[0]: 0;
-            } else {
-                return 0;
-            }
+        if (prices.length < 2) {
+            return 0;
         }
-        return 0;
+        int buy = 0 - prices[0];
+        int cool = 0;
+        int sell = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int temp = buy;
+            buy = buy > cool - prices[i]? buy: cool - prices[i];
+            cool = sell > cool? sell: cool;
+            sell = sell > temp + prices[i]? sell: temp + prices[i];
+        }
+        return sell > cool? sell: cool;
     }
-    /**************************** Climbing Stairs ****************************/
+    /**************************** 70. Climbing Stairs ****************************/
     /**
      * You are climbing a stair case. It takes n steps to reach to the top.
      * Each time you can either climb 1 or 2 steps. 
@@ -34,7 +39,16 @@ public class RealProblems {
      * @return
      */
     public int climbStairs(int n) {
-        return 0;
+        if (n < 2) {
+            return 1;
+        }
+        int[] steps = new int[n + 1];
+        steps[0] = 1;
+        steps[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+        return steps[n];
     }
     /**************************** Best Time to Buy and Sell Stock ****************************/
     /**
@@ -99,26 +113,35 @@ public class RealProblems {
     /**************************** Main ****************************/
     public static void main(String[] args) {
         RealProblems problems = new RealProblems();
-//        problems.testMaxProfitCool();
+        problems.testMaxProfitCool();
 //        problems.testClimbStairs();
 //        problems.testMaxProfit();
-        problems.testMaxProfitIII();
+//        problems.testMaxProfitIII();
     }
     /**************************** Testing Methods ****************************/
     public void testMaxProfitCool() {
+        String output = "expected: %d\tresult: %d\n";
         int[] prices00 = {1, 2, 3, 0, 2};
-        this.print(this.maxProfitCool(prices00));
+        System.out.format(output, 3, this.maxProfitCool(prices00));
         int[] prices01 = {};
-        this.print(this.maxProfitCool(prices01));
+        System.out.format(output, 0, this.maxProfitCool(prices01));
         int[] prices02 = {1};
-        this.print(this.maxProfitCool(prices02));
+        System.out.format(output, 0, this.maxProfitCool(prices02));
         int[] prices03 = {1, 2, 3};
-        this.print(this.maxProfitCool(prices03));
+        System.out.format(output, 2, this.maxProfitCool(prices03));
         int[] prices04 = {1, 2, 3, 0};
-        this.print(this.maxProfitCool(prices04));
+        System.out.format(output, 2, this.maxProfitCool(prices04));
+        int[] prices05 = {1, 2, 3, 0, 2, 2, 3, 0, 2};
+        System.out.format(output, 5, this.maxProfitCool(prices05));
+        int[] prices06 = {1, 2, 3, 0, 2, 2, 3, 0, 2, 2, 3, 0, 2};
+        System.out.format(output, 7, this.maxProfitCool(prices06));
+        int[] prices07 = {1, 2, 3, 0, 2, 2, 3, 0, 2, 2, 3, 0, 2, 2, 3, 0, 2, 2, 3, 0, 2, 2, 3, 0, 2};
+        System.out.format(output, 13, this.maxProfitCool(prices07));
     }
     public void testClimbStairs() {
-        System.out.println("Hello Dynamic Programming.");
+        for (int i = 0; i < 46; i++) {
+            System.out.println(i + " steps, " + this.climbStairs(i) + " ways to climb.");
+        }
     }
     public void testMaxProfit() {
         int[] prices00 = {1, 2, 3, 0, 2};
@@ -170,6 +193,15 @@ public class RealProblems {
             return;
         }
         for (char item: array) {
+            System.out.print(item + ", ");
+        }
+        System.out.println();
+    }
+    public void print(boolean[] array) {
+        if (array == null) {
+            return;
+        }
+        for (boolean item: array) {
             System.out.print(item + ", ");
         }
         System.out.println();
