74ef10e71076dc553d6bfa13940f520cabd0b096
Han Na <hosseni@hotmail.com>
2016-01-04 02:45:18 -0500

    123. Best Time to Buy and Sell Stock III Accepted. Win

--- DynamicProgramming/src/RealProblems.java
+++ DynamicProgramming/src/RealProblems.java
@@ -16,6 +16,13 @@ public class RealProblems {
      * @return
      */
     public int maxProfitCool(int[] prices) {
+        if (prices == null || prices.length <= 2) {
+            if (prices.length == 2) {
+                return prices[1] - prices[0] > 0? prices[1] - prices[0]: 0;
+            } else {
+                return 0;
+            }
+        }
         return 0;
     }
     /**************************** Climbing Stairs ****************************/
@@ -39,12 +46,52 @@ public class RealProblems {
      * @return
      */
     public int maxProfit(int[] prices) {
-        return 0;
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxProfit = prices[0] - prices[0];
+        int minPrice = prices[0];
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = prices[i] < minPrice? prices[i]: minPrice;
+            maxProfit = prices[i] - minPrice > maxProfit? prices[i] - minPrice: maxProfit;
+        }
+        return maxProfit;
     }
     /**************************** House Robber ****************************/
     /**************************** House Robber II ****************************/
     /**************************** Burst Balloons ****************************/
-    /**************************** Best Time to Buy and Sell Stock III ****************************/
+    /**************************** 123. Best Time to Buy and Sell Stock III ****************************/
+    /**
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * Design an algorithm to find the maximum profit. You may complete at most TWO transactions.
+     * Note:
+     * You may not engage in multiple transactions at the same time 
+     * (ie, you must sell the stock before you buy again).
+     * @param prices
+     * @return
+     */
+    public int maxProfitIII(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int[] first = new int[prices.length];
+        int[] second = new int[prices.length];
+        int minPrice = prices[0];
+        int maxPrice = prices[prices.length - 1];
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = minPrice < prices[i]? minPrice: prices[i];
+            first[i] = first[i - 1] > prices[i] - minPrice? first[i - 1]: prices[i] - minPrice;
+        }
+        for (int i = prices.length - 2; i >= 0; i--) {
+            maxPrice = maxPrice > prices[i]? maxPrice: prices[i];
+            second[i] = second[i + 1] > maxPrice - prices[i]? second[i + 1]: maxPrice - prices[i];
+        }
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            maxProfit = maxProfit > first[i] + second[i]? maxProfit: first[i] + second[i];
+        }
+        return maxProfit;
+    }
     /**************************** Best Time to Buy and Sell Stock IV ****************************/
     /**************************** Dungeon Game ****************************/
     /**************************** Decode Ways ****************************/
@@ -52,20 +99,52 @@ public class RealProblems {
     /**************************** Main ****************************/
     public static void main(String[] args) {
         RealProblems problems = new RealProblems();
-        problems.testMaxProfitCool();
-        problems.testClimbStairs();
-        problems.testMaxProfit();
+//        problems.testMaxProfitCool();
+//        problems.testClimbStairs();
+//        problems.testMaxProfit();
+        problems.testMaxProfitIII();
     }
     /**************************** Testing Methods ****************************/
     public void testMaxProfitCool() {
         int[] prices00 = {1, 2, 3, 0, 2};
         this.print(this.maxProfitCool(prices00));
+        int[] prices01 = {};
+        this.print(this.maxProfitCool(prices01));
+        int[] prices02 = {1};
+        this.print(this.maxProfitCool(prices02));
+        int[] prices03 = {1, 2, 3};
+        this.print(this.maxProfitCool(prices03));
+        int[] prices04 = {1, 2, 3, 0};
+        this.print(this.maxProfitCool(prices04));
     }
     public void testClimbStairs() {
         System.out.println("Hello Dynamic Programming.");
     }
     public void testMaxProfit() {
-        System.out.println("Hello Dynamic Programming.");
+        int[] prices00 = {1, 2, 3, 0, 2};
+        this.print(this.maxProfit(prices00));
+        int[] prices01 = {};
+        this.print(this.maxProfit(prices01));
+        int[] prices02 = {1};
+        this.print(this.maxProfit(prices02));
+        int[] prices03 = {1, 2, 3};
+        this.print(this.maxProfit(prices03));
+        int[] prices04 = {1, 2, 3, 0};
+        this.print(this.maxProfit(prices04));
+    }
+    public void testMaxProfitIII() {
+        int[] prices00 = {1, 2, 3, 0, 2};
+        this.print(this.maxProfitIII(prices00));
+        int[] prices01 = {};
+        this.print(this.maxProfitIII(prices01));
+        int[] prices02 = {1};
+        this.print(this.maxProfitIII(prices02));
+        int[] prices03 = {1, 2, 3};
+        this.print(this.maxProfitIII(prices03));
+        int[] prices04 = {1, 2, 3, 0};
+        this.print(this.maxProfitIII(prices04));
+        int[] prices05 = {6, 1, 3, 2, 4, 7};
+        this.print(this.maxProfitIII(prices05));
     }
     /**************************** Printing Methods ****************************/
     public void print(int n) {
