ac5063606573d457d24e5c94f32c30b30d48b034
Han Na <hosseni@hotmail.com>
2016-01-10 18:16:22 -0500

    322. Coin Change Accepted. Win this solution is based on discuss.

--- DynamicProgramming/src/RealProblems.java
+++ DynamicProgramming/src/RealProblems.java
@@ -183,20 +183,40 @@ public class RealProblems {
         }
         return maxProfit;
     }
+    /**************************** 322. Coin Change ****************************/
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount == 0) {
+            return 0;
+        }
+        int[] result = new int[amount + 1];
+        result[0] = 0;
+        for (int i = 1; i < result.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                int sub = i - coins[j];
+                if ((sub > 0 && result[sub] > 0) || sub == 0) {
+                    min = Math.min(min, result[sub] + 1);
+                }
+            }
+            result[i] = min == Integer.MAX_VALUE? -1: min;
+        }
+        return result[amount];
+    }
     /**************************** Best Time to Buy and Sell Stock IV ****************************/
     /**************************** Dungeon Game ****************************/
     /**************************** Decode Ways ****************************/
     /**************************** Wildcard Matching ****************************/
     /**************************** Main ****************************/
     public static void main(String[] args) {
-        RealProblems problems = new RealProblems();
-//        problems.testMaxProfitCool();
-//        problems.testClimbStairs();
-//        problems.testMaxProfit();
-//        problems.testRob();
-        problems.testRobII();
-//        problems.testMaxCoins();
-//        problems.testMaxProfitIII();
+        RealProblems solution = new RealProblems();
+//        solution.testMaxProfitCool();
+//        solution.testClimbStairs();
+//        solution.testMaxProfit();
+//        solution.testRob();
+//        solution.testRobII();
+//        solution.testMaxCoins();
+//        solution.testMaxProfitIII();
+        solution.testCoinChange();
     }
     /**************************** Testing Methods ****************************/
     public void testMaxProfitCool() {
@@ -318,6 +338,21 @@ public class RealProblems {
         int[] prices05 = {6, 1, 3, 2, 4, 7};
         this.print(this.maxProfitIII(prices05));
     }
+    public void testCoinChange() {
+        String output = "coins: %s\tamout: %d\texpected: %d\tresult: %d\n";
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.format(output, this.arrayString(coins), amount, 3, this.coinChange(coins, amount));
+        int[] coins1 = {2};
+        amount = 3;
+        System.out.format(output, this.arrayString(coins1), amount, -1, this.coinChange(coins1, amount));
+        int[] coins2 = {2};
+        amount = 1;
+        System.out.format(output, this.arrayString(coins2), amount, -1, this.coinChange(coins2, amount));
+        int[] coins3 = {186, 419, 83, 408};
+        amount = 6249;
+        System.out.format(output, this.arrayString(coins3), amount, 20, this.coinChange(coins3, amount));
+    }
     /**************************** Printing Methods ****************************/
     public void print(int n) {
         System.out.println(n);
@@ -360,5 +395,18 @@ public class RealProblems {
     }
     public void printList(List<String> list) {
         System.out.println(list);
+    }
+    public String arrayString(int[] array) {
+        if (array == null) {
+            return "null";
+        } else {
+            StringBuilder builder = new StringBuilder();
+            builder.append('[');
+            for (int i = 0; i < array.length; i++) {
+                builder.append(array[i] + " ");
+            }
+            builder.append(']');
+            return builder.toString();
+        }
     }
 }
