62472205be9627bb7c60f7af3ae43753d9a14ab7
Han Na <hosseni@hotmail.com>
2016-01-12 19:18:20 -0500

    Accepted. Win

---
+++ LeetCode287/src/Solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+}
