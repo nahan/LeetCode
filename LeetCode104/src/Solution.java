b9e0319b272c92f569fc0ee656758d3eb31326ad
Han Na <hosseni@hotmail.com>
2016-03-06 20:20:07 -0500

    Accepted. 2nd Round.

--- LeetCode104/src/Solution.java
+++ LeetCode104/src/Solution.java
@@ -13,13 +13,12 @@ public class Solution {
 	 * Given a binary tree, find its maximum depth.
 	 * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 	 */
-	public int maxDepth(TreeNode root) {
-		if (root == null) {
-			return 0;
-		} else {
-			return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-		}
-	}
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		TreeNode node1 = new TreeNode(1);
