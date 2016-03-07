9f507479a4e3f1b0ebe29046a2ce7424b47e0c04
Han Na <hosseni@hotmail.com>
2016-03-06 20:34:48 -0500

    Accepted. 2nd Round.

--- LeetCode219/src/Solution.java
+++ LeetCode219/src/Solution.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 
@@ -20,6 +21,18 @@ public class Solution {
                 return true;
             } else {
                 hash.add(nums[index]);
+            }
+        }
+        return false;
+    }
+    public boolean containsDuplicate2(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
             }
         }
         return false;
