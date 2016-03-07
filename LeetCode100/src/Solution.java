314b7ecc169c332a70162589ed823a8c647c3c1c
Han Na <hosseni@hotmail.com>
2016-03-06 20:28:09 -0500

    Accepted. 2nd Round.

--- LeetCode100/src/Solution.java
+++ LeetCode100/src/Solution.java
@@ -14,20 +14,19 @@ public class Solution {
 	 * Given two binary trees, write a function to check if they are equal or not.
 	 * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
 	 */
-	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if ((p == null && q != null) || (p != null && q == null)) {
-			return false;
-		} else {
-			if (p == null && q == null) {
-				return true;
-			}
-			if (p.val == q.val) {
-				return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-			} else {
-				return false;
-			}
-		}
-	}
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        } else if (p == null && q == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		TreeNode node1 = new TreeNode(1);
